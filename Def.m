74d68ef0331d9377dc38e75d4d4623990e0f7578
kyo <phongbinh2511@gmail.com>
2020-11-29 21:18:55 +0800

    [master] Tuned parameters for the network.

--- Def.m
+++ Def.m
@@ -1,7 +1,7 @@
 classdef Def
     properties (Constant)
-        TRAIN_ROUNDS_N = 10 % [N]umber of [TRAIN] [ROUNDS]
-        LEARNING_RATE = 0.00006
+        TRAIN_ROUNDS_N = 50 % [N]umber of [TRAIN] [ROUNDS]
+        LEARNING_RATE = 0.006
         MINI_BATCH_LENGTH = 10
         GRAY_N = 255
     end
