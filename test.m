97da14ecd3b496ab7bd740178733824ff550c340
kyo <phongbinh2511@gmail.com>
2021-05-31 22:40:35 +0800

    [master] Made clear constant variables.

--- test.m
+++ test.m
@@ -1,16 +1,16 @@
 % input
-test_data = load('mnist_test.csv');
-test_data_n = numel( test_data(:, 1) );
-test_in = normalized_grayscale( test_data(:, 2:785) );
-test_ou = test_data(:, 1);
+TEST_DATA = load('mnist_test.csv');
+TEST_DATA_N = numel( TEST_DATA(:, 1) );
+TEST_IN = normalized_grayscale( TEST_DATA(:, 2:785) );
+TEST_OU = TEST_DATA(:, 1);
 load('trained_parameters', 'layers');
 LAYERS_N = numel(layers);
 layers_associates(LAYERS_N, 1) = layer_associates;
 
 % test
 correct_n = 0;
-for test_data_i = 1:test_data_n
-	layers_associates(1).y = transpose( test_in(test_data_i, :) );
+for test_data_i = 1:TEST_DATA_N
+	layers_associates(1).y = transpose( TEST_IN(test_data_i, :) );
     for l_i = 2:LAYERS_N
         layers_associates(l_i).z = layers(l_i).w * layers_associates(l_i-1).y + layers(l_i).b;
         layers_associates(l_i).y = f( layers_associates(l_i).z );
@@ -25,10 +25,10 @@ for test_data_i = 1:test_data_n
         end
     end
     
-    if out_predict == test_ou(test_data_i)
+    if out_predict == TEST_OU(test_data_i)
         correct_n = correct_n + 1;
     end
 end
 
 % output
-disp( ['accuracy: ', num2str(correct_n / test_data_n * 100), '%'] );
+disp( ['accuracy: ', num2str(correct_n / TEST_DATA_N * 100), '%'] );
