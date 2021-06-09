611a82b9bf9b19686982c740286eeb072bffc9ae
kyo <phongbinh2511@gmail.com>
2021-06-09 12:21:01 +0800

    [master] Replaced use of struct `layer_associates` with static variables. Computational time is now 511.7134 seconds (with profile on). A great leap!

--- train_fast.m
+++ train_fast.m
@@ -15,7 +15,6 @@ b4 = readmatrix('b4');
 w2 = readmatrix('w2');
 w3 = readmatrix('w3');
 w4 = readmatrix('w4');
-layers_associates(LAYERS_N, 1) = layer_associates;
 MINI_BATCH_LEARNING_RATE = nn.LEARNING_RATE / nn.MINI_BATCH_LENGTH;
 
 %% train
@@ -23,12 +22,12 @@ tic;
 for train_round_i = 1:nn.TRAIN_ROUNDS_N
     for mini_batch_i = 1:(TRAIN_DATA_N / nn.MINI_BATCH_LENGTH)
         % initialize
-        layers_associates(2).dcdb_cum = zeros( size( b2 ) );
-        layers_associates(3).dcdb_cum = zeros( size( b3 ) );
-        layers_associates(4).dcdb_cum = zeros( size( b4 ) );
-        layers_associates(2).dcdw_cum = zeros( size( w2 ) );
-        layers_associates(3).dcdw_cum = zeros( size( w3 ) );
-        layers_associates(4).dcdw_cum = zeros( size( w4 ) );
+        dcdb_cum2 = zeros( size( b2 ) );
+        dcdb_cum3 = zeros( size( b3 ) );
+        dcdb_cum4 = zeros( size( b4 ) );
+        dcdw_cum2 = zeros( size( w2 ) );
+        dcdw_cum3 = zeros( size( w3 ) );
+        dcdw_cum4 = zeros( size( w4 ) );
 
         start_i = (mini_batch_i - 1) * nn.MINI_BATCH_LENGTH + 1;
         end_i = start_i + nn.MINI_BATCH_LENGTH - 1;
@@ -36,38 +35,38 @@ for train_round_i = 1:nn.TRAIN_ROUNDS_N
             desired_output_layer = one_hot_vector( TRAIN_OU(train_data_i) );
 
             % feedforward
-            layers_associates(1).y = transpose( TRAIN_IN(train_data_i, :) );
-            layers_associates(2).z = w2 * layers_associates(1).y + b2;
-            layers_associates(2).y = f( layers_associates(2).z );
-            layers_associates(3).z = w3 * layers_associates(2).y + b3;
-            layers_associates(3).y = f( layers_associates(3).z );
-            layers_associates(4).z = w4 * layers_associates(3).y + b4;
-            layers_associates(4).y = f( layers_associates(4).z );
+            y1 = transpose( TRAIN_IN(train_data_i, :) );
+            z2 = w2 * y1 + b2;
+            y2 = f( z2 );
+            z3 = w3 * y2 + b3;
+            y3 = f( z3 );
+            z4 = w4 * y3 + b4;
+            y4 = f( z4 );
 
             % backpropagation
-            dcdb = ( 2 * ( layers_associates(4).y - desired_output_layer ) ) .* dfdz( layers_associates(4).z );
-            dcdw = dcdb * transpose( layers_associates(3).y );
-            layers_associates(4).dcdb_cum = layers_associates(4).dcdb_cum + dcdb;
-            layers_associates(4).dcdw_cum = layers_associates(4).dcdw_cum + dcdw;
+            dcdb = ( 2 * ( y4 - desired_output_layer ) ) .* dfdz( z4 );
+            dcdw = dcdb * transpose( y3 );
+            dcdb_cum4 = dcdb_cum4 + dcdb;
+            dcdw_cum4 = dcdw_cum4 + dcdw;
             
-            dcdb = ( transpose( w4 ) * dcdb ) .* dfdz( layers_associates(3).z );
-            dcdw = dcdb * transpose( layers_associates(2).y );
-            layers_associates(3).dcdb_cum = layers_associates(3).dcdb_cum + dcdb;
-            layers_associates(3).dcdw_cum = layers_associates(3).dcdw_cum + dcdw;
+            dcdb = ( transpose( w4 ) * dcdb ) .* dfdz( z3 );
+            dcdw = dcdb * transpose( y2 );
+            dcdb_cum3 = dcdb_cum3 + dcdb;
+            dcdw_cum3 = dcdw_cum3 + dcdw;
             
-            dcdb = ( transpose( w3 ) * dcdb ) .* dfdz( layers_associates(2).z );
-            dcdw = dcdb * transpose( layers_associates(1).y );
-            layers_associates(2).dcdb_cum = layers_associates(2).dcdb_cum + dcdb;
-            layers_associates(2).dcdw_cum = layers_associates(2).dcdw_cum + dcdw;
+            dcdb = ( transpose( w3 ) * dcdb ) .* dfdz( z2 );
+            dcdw = dcdb * transpose( y1 );
+            dcdb_cum2 = dcdb_cum2 + dcdb;
+            dcdw_cum2 = dcdw_cum2 + dcdw;
         end
 
         % update
-        b2 = b2 - layers_associates(2).dcdb_cum * MINI_BATCH_LEARNING_RATE;
-        b3 = b3 - layers_associates(3).dcdb_cum * MINI_BATCH_LEARNING_RATE;
-        b4 = b4 - layers_associates(4).dcdb_cum * MINI_BATCH_LEARNING_RATE;
-        w2 = w2 - layers_associates(2).dcdw_cum * MINI_BATCH_LEARNING_RATE;
-        w3 = w3 - layers_associates(3).dcdw_cum * MINI_BATCH_LEARNING_RATE;
-        w4 = w4 - layers_associates(4).dcdw_cum * MINI_BATCH_LEARNING_RATE;
+        b2 = b2 - dcdb_cum2 * MINI_BATCH_LEARNING_RATE;
+        b3 = b3 - dcdb_cum3 * MINI_BATCH_LEARNING_RATE;
+        b4 = b4 - dcdb_cum4 * MINI_BATCH_LEARNING_RATE;
+        w2 = w2 - dcdw_cum2 * MINI_BATCH_LEARNING_RATE;
+        w3 = w3 - dcdw_cum3 * MINI_BATCH_LEARNING_RATE;
+        w4 = w4 - dcdw_cum4 * MINI_BATCH_LEARNING_RATE;
     end
 end
 t = toc;
